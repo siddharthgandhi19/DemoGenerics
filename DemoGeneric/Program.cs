edfdab0b1ac3d783e2a4ec352d52ef9fb15cff20
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-15 14:56:33 +0530

    [Siddharth]Add: Create single method for multiple datatypes

--- DemoGeneric/Program.cs
+++ DemoGeneric/Program.cs
@@ -5,13 +5,17 @@ namespace DemoGeneric
     {
         static void Main(string[] args)
         {
-            int[] intArr = { 1, 2, 3, 4, 5, 6, 7 };
-            double[] doubleArr = { 1.02, 2.03000, 3.05, 4.022 };
-            char[] charArr = { 'a', 'b', 'c', 'd', 'e', };
-            GenericOperation genericOperation = new GenericOperation();
-            genericOperation.ToPrintArray(intArr);
-            genericOperation.ToPrintArray(doubleArr);
-            genericOperation.ToPrintArray(charArr);
+            int[] intArr = { 1, 2, 3, 4, 5 };
+            double[] doubleArr = { 1.0, 2.0, 3.0, 4.0, 5.0 };
+            char[] charArr = { 'a', 'b', 'c', 'd', 'e' };
+            GenericOperation generic = new GenericOperation();
+            generic.ToPrintArray<int>(intArr);
+            generic.ToPrintArray<double>(doubleArr);
+            generic.ToPrintArray<char>(charArr);
+
+            /*generic.ToPrintArray(intArr);
+             generic.ToPrintArray(doubleArr);
+             generic.ToPrintArray(charArr);*/
         }
     }
 }
