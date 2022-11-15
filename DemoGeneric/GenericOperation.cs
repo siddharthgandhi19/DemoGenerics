824a65d8fef84e32a0c75df0644ea0a17c9dce3c
siddharth gandhi <siddharthgandhi@outlook.com>
2022-11-15 14:45:56 +0530

    [Siddharth]Add: Create multiple methods for multiple datatypes

---
+++ DemoGeneric/GenericOperation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+namespace DemoGeneric
+{
+    public class GenericOperation
+    {
+         public void ToPrintArray(int[] intArr)
+         {
+             foreach (var data in intArr)
+             {
+                 Console.WriteLine(data);
+             }
+         }
+
+         public void ToPrintArray(double [] intArr) // we initialise intArr we can also use doubleArr or anything
+         {
+             foreach (var data in intArr)
+             {
+                 Console.WriteLine(data);
+             }
+         }
+
+         public void ToPrintArray(char[] intArr)
+         {
+             foreach (var data in intArr)
+             {
+                 Console.WriteLine(data);
+             }
+         } 
+    }
+}
---
+++ DemoGeneric/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace DemoGeneric
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] intArr = { 1, 2, 3, 4, 5, 6, 7 };
+            double[] doubleArr = { 1.02, 2.03000, 3.05, 4.022 };
+            char[] charArr = { 'a', 'b', 'c', 'd', 'e', };
+            GenericOperation genericOperation = new GenericOperation();
+            genericOperation.ToPrintArray(intArr);
+            genericOperation.ToPrintArray(doubleArr);
+            genericOperation.ToPrintArray(charArr);
+        }
+    }
+}
